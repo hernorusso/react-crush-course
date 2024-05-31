b51fa9832591dc0a16b06c02c8366680817bbd25
herno <herno.russo@gmail.com>
2024-05-31 10:52:17 -0300

    Imporve code semantic
    Follow the convenction <Component onX={xHandler}/>

--- src/app/pages/components/modal/modal.jsx
+++ src/app/pages/components/modal/modal.jsx
@@ -2,20 +2,20 @@ import { useNavigate } from 'react-router-dom';
 
 import styles from './modal.module.css';
 
-const stopClickPopagation = (e) => {
+const clickPopagationHandler = (e) => {
   e.stopPropagation();
 };
 
 const Modal = ({ children }) => {
   const navigateTo = useNavigate();
 
-  const onClick = () => {
+  const clickHandler = () => {
     navigateTo('..');
   };
 
   return (
-    <div className={styles.backdrop} onClick={onClick}>
-      <dialog open className={styles.modal} onClick={stopClickPopagation}>
+    <div className={styles.backdrop} onClick={clickHandler}>
+      <dialog open className={styles.modal} onClick={clickPopagationHandler}>
         {children}
       </dialog>
     </div>
